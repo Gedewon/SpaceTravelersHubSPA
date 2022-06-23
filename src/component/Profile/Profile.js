14509c2bc26407d4c34c36e272db253bd24c85bd
gdumani <gdumani@gmail.com>
2022-06-23 16:36:01 -0600

    add joined missions to profile page

--- src/component/Profile/Profile.js
+++ src/component/Profile/Profile.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-function Profile() {
+const Profile = () => {
+  const missions = useSelector((state) => state.missionsReducer);
+  const joined = missions.filter((mission) => mission.missionStatus);
+
   return (
-    <div>Profile</div>
+    <div>
+      <h2>My Missions</h2>
+      <table>
+        <tbody>
+          {joined.map((item) => (
+            <tr key={item.missionId}>
+              <td>{item.missionName}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
-}
+};
 
 export default Profile;
