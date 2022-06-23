2043a84cd935645778dd5fe39d4a69127a178cab
gdumani <gdumani@gmail.com>
2022-06-23 16:46:02 -0600

    add style wapper

--- src/component/Profile/Profile.js
+++ src/component/Profile/Profile.js
@@ -1,23 +1,57 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 
 const Profile = () => {
   const missions = useSelector((state) => state.missionsReducer);
   const joined = missions.filter((mission) => mission.missionStatus);
 
+  const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  text-align: left;
+  column-gap: 2em;
+  padding: 1em 2em;
+
+  div {
+    width: 50%;
+  }
+
+  table {
+    width: 100%;
+  }
+
+  td {
+    padding: 1em;
+  }
+
+  tr {
+    outline: 1px solid black;
+  }
+`;
+
   return (
-    <div>
-      <h2>My Missions</h2>
-      <table>
-        <tbody>
-          {joined.map((item) => (
-            <tr key={item.missionId}>
-              <td>{item.missionName}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Wrapper>
+      <div>
+        <h2>My Missions</h2>
+        <table>
+          <tbody>
+            {joined.map((item) => (
+              <tr key={item.missionId}>
+                <td>{item.missionName}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <h2>My Rockets</h2>
+        <table>
+          {/* code for booked Rockets */}
+          <tbody />
+        </table>
+      </div>
+    </Wrapper>
   );
 };
 
