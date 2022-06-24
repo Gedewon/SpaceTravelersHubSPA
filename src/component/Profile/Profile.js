e97ca6f57528cae0916ad56c5d2c6d98451fe42b
gedewon <gedewon.hayle.9@gmail.com>
2022-06-24 17:54:40 +0300

    add profile section

--- src/component/Profile/Profile.js
+++ src/component/Profile/Profile.js
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 
 const Profile = () => {
   const missions = useSelector((state) => state.missionsReducer);
+  const rockets = useSelector((state) => state.rocketsReducer);
   const joined = missions.filter((mission) => mission.missionStatus);
+  const reserved = rockets.filter((rocket)=>rocket.rocketStatus);
 
   const Wrapper = styled.div`
   display: flex;
@@ -47,8 +49,16 @@ const Profile = () => {
       <div>
         <h2>My Rockets</h2>
         <table>
-          {/* code for booked Rockets */}
-          <tbody />
+        <tbody>
+          {
+            reserved.map((item)=>(
+
+              <tr key={item.rocketId}>
+              <td>{item.rocketName}</td>
+            </tr>
+            ))
+          }
+          </tbody >
         </table>
       </div>
     </Wrapper>
